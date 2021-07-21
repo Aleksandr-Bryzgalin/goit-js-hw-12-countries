8aded94022263922b4ff161cf0fe0374990e43b1
Aleksandr-Bryzgalin <aleksandr.bryzgalin@hotmail.com>
2021-07-21 15:13:25 +0300

    правка ошибок ошибок

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ import cardTpl from './templates/countrie-card-markup.hbs';
 import listTpl from './templates/countrie-list-markup.hbs';
 
 
-// Проблема с Pnotify
-
-// import { alert, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
-// import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-// import { alert, notice, info, success, error } from '@pnotify/core';
-
-
-
 const refs = {
     cardContainerRef: document.querySelector('.js-card-container'),
     inputRef: document.querySelector('.js-input')
@@ -24,57 +16,37 @@ var debounce = require('lodash.debounce');
 refs.inputRef.addEventListener('input', debounce(onSearch, 500))
 
 
-function clearSearch() {
-  refs.cardContainerRef.innerHTML = '';
-}
-
 function onSearch(e) {    
     e.preventDefault();
-    clearSearch();
+       
+    const searchInput = e.target.value;
     
-    const searchInput = refs.inputRef.value;    
+    if (searchInput === "") {
+       return
+    }
+
+    if (searchInput.status === 404) {
+        return myError();
+    }
    
     fetchApi(searchInput)
         .then(renderResult)
-        .catch((error) => {
-            return error = alert('Oh No! Something terrible happened.');
-        })
-    
-        // Проблема с Pnotify
-    
-        // .catch(error => console.log(error))
-        // .finally(() => clearSearch());
-        
+        .catch(myError)
+            
 }
 
 function renderResult(countries) {
-   
-     if (countries.length === 1) {
-        clearSearch();
+    
+    if (countries.length === 1) {        
         renderCountryCard(countries)
-    } else if (countries.length > 1 && countries.length <= 10) {
-        clearSearch();
+    } else if (countries.length > 1 && countries.length <= 10) {   
         renderCountrysList(countries);
-     } else if (countries.length > 10) {
-         return alert('Oh! To many matches found! Clarify your request!')
-         
-         // Проблема с Pnotify
-
-        //  error({
-        //      title: 'Oh! To many matches found!',
-        //      text: 'Clarify your request!'
-        //  });    
+    } else {
+        myError(error);      
+          
     }
 }
 
-// Проблема с Pnotify
-
-// function myError() {
-//     error({
-//         title: 'Oh No!',
-//         text: 'Something terrible happened.'
-//     });
-// }
 
 function renderCountryCard(country) {
     const markup = cardTpl(country)
@@ -87,5 +59,8 @@ function renderCountrysList(contrys) {
     refs.cardContainerRef.innerHTML = markupList;
 }
 
+function myError(error) {
+    alert('Oh No! Something terrible happened. Clarify your request!');
+}
 
 
